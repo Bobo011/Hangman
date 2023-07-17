2146019e7f0e47784d61b26eece8b83629f86fff
George <alximik15@gmail.com>
2023-07-17 19:32:35 +0400

    Created basic component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import words from "./wordList.json";
+import {HangmanWord,HangmanDrawing,Keyboard } from './components'
 function App() {
+  const [wordToGuess, setwordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
 
+  const [guessedLetters, setguessedLetters] = useState<string[]>([]);
 
-  return 
+  console.log(wordToGuess);
+
+  return (
+    <div
+      style={{
+        maxWidth: " 800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export {HangmanWord} from './HangmanWord'
+export {HangmanDrawing} from './HangmanDrawing'
+export {Keyboard} from './Keyboard'
