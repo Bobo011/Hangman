94eadc1a6ea4ee592d6a869fa8ab9bed4ceabae9
George <alximik15@gmail.com>
2023-07-17 20:12:25 +0400

    HangmanWord finished

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
-
 export const HangmanWord = () => {
+  const word = "test";
+const guessedLetters =['t']
   return (
-    <div>HangmanWord</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fonWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span key={index} style={{ borderBottom: ".1em solid black" }}>
+          <span style={{visibility:guessedLetters.includes(letter) ? 'visible':'hidden'}}>{letter}</span>
+        </span>
+      ))}
+    </div>
+  );
+};
